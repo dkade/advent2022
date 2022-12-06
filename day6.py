52ffc85f46c1bfecb2e3e14a72caf1efb5bfd0b3
Daniel Loureiro <daniel.loureiro@olx.com>
2022-12-06 12:10:48 +0000

    Missing second part of day6

--- day6.py
+++ day6.py
@@ -7,21 +7,25 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 signal = sys.argv[1]
-marker = []
-marker_count = 0
+
+def get_start(signal, num_char):
+    marker = []
+    marker_count = 0
+    for index, char in enumerate(signal):
+        # print("char: {} index: {} marker: {}".format(char, index, marker))
+        if char not in marker and marker_count < num_char:
+            marker.append(char)
+            marker_count +=1
+            if marker_count == num_char:
+                # print ("start marker" + str(index+1))
+                return index+1
+        else:
+            repeated_char_index = marker.index(char)
+            del marker[0:repeated_char_index+1]
+            marker.append(char)
+            marker_count = len(marker)
+            # print("r_c_i: {} count: {} marker: {}".format(repeated_char_index, marker_count, marker))
 
 
-for index, char in enumerate(signal):
-    # print("char: {} index: {} marker: {}".format(char, index, marker))
-    if char not in marker and marker_count < 4:
-        marker.append(char)
-        marker_count +=1
-        if marker_count == 4:
-            print ("Start of Packet: " + str(index+1))
-            break
-    else:
-        repeated_char_index = marker.index(char)
-        del marker[0:repeated_char_index+1]
-        marker.append(char)
-        marker_count = len(marker)
-        # print("r_c_i: {} count: {} marker: {}".format(repeated_char_index, marker_count, marker))
+print("Start of marker: " + str(get_start(signal, 4)))
+print("Start of message: " + str(get_start(signal, 14)))
